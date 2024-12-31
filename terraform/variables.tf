8b639795d97443a4627f6c09e9968bf5fd351121
DaveTheBearMan <DaveTheBearMan@gmail.com>
2024-12-31 19:08:49 +0000

    Further set up, added versions.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,20 @@
 # Openstack credentials 
 variable "username" {
-    description = "Openstack Username"
-    type = string
+  description = "Openstack Username"
+  type        = string
 }
 
 variable "password" {
-    description = "Openstack Password"
-    type = string
+  description = "Openstack Password"
+  type        = string
 }
 
 variable "auth_url" {
-    description = "Openstack authentication url"
-    type = string
+  description = "Openstack authentication url"
+  type        = string
 }
 
 variable "tenant_name" {
-    description = "Openstack project name"
-    type = string
+  description = "Openstack project name"
+  type        = string
 }
