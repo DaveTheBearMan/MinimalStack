208e8dbc098d87fad6fd8a48dfe492c4090a4329
DaveTheBearMan <DaveTheBearMan@gmail.com>
2024-12-31 18:43:53 +0000

    Structured terraform

--- backend/go.mod
+++
@@ -1,7 +0,0 @@
-module minimalstack
-
-go 1.23.1
-
-require github.com/gophercloud/gophercloud/v2 v2.2.0
-
-require gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+# Openstack credentials 
+variable "username" {
+    description = "Openstack Username"
+    type = string
+}
+
+variable "password" {
+    description = "Openstack Password"
+    type = string
+}
+
+variable "auth_url" {
+    description = "Openstack authentication url"
+    type = string
+}
+
+variable "tenant_name" {
+    description = "Openstack project name"
+    type = string
+}
